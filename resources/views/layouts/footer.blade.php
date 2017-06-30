1e34889ae5ed74d53b5e8c80d519fdd6eca50b51
Rafael Medina <rafaelmedina@outlook.com>
2017-06-30 13:41:21 -0400

    welcome

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="pv4 ph3 ph5-m ph6-l">
+  <small class="f6 db tc">© 2017 <b class="ttu">Stark Ai LABS</b>., All Rights Reserved</small>
+  <div class="tc mt3">
+    <a href="/language/" title="Language" class="f6 dib ph2 link  dim">Language</a>
+    <a href="/terms/"    title="Terms" class="f6 dib ph2 link dim">Terms of Use</a>
+    <a href="/privacy/"  title="Privacy" class="f6 dib ph2 link  dim">Privacy</a>
+  </div>
+</footer>
