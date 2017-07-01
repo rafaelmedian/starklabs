dd90daeec2b2ba54bf993bcd2c144ade30ac5790
Danny Feliz <danny@mctekk.com>
2017-07-01 01:16:40 -0400

    Add email class

---
+++ app/Mail/JobApplication.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class JobApplication extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $jobApplication;
+
+    /**
+     * Create a new message instance.
+     * @param $jobApplication
+     */
+    public function __construct($jobApplication)
+    {
+        $this->jobApplication = $jobApplication;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+
+
+        return $this->to($this->jobApplication->email, $this->jobApplication->full_name)
+                     ->view('emails.jobapplication');
+    }
+}
