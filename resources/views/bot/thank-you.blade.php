57504b8164bc281f002da05cbb052fc3c4b73d96
Rafael Medina <rafaelmedina@outlook.com>
2018-10-19 19:11:04 -0400

    thank you bot

--- resources/views/bot/thank-you.blade.php
+++ resources/views/bot/thank-you.blade.php
@@ -7,9 +7,11 @@
   <div class="mb6 pa3 pa0-l tc">
     <h1 class="mb3 f2">Thank you. We will contact you very soon.</h1>
     <p class="mb5">Keep having fun meanwhile, and spread the word.</p>
-    <a href="{{ secure_url('/') }}" class="grow f6 f5-ns tc-m dib br-pill ba ph4 pv2 cta"
-    ><img class="mr2 v-mid" src="{{ secure_asset('/dist/img/l-arrow.svg') }}" alt="go bg-near-black"> Go Back </a>
+    <a href="{{ secure_url('/') }}" class="grow f6 f5-ns tc-m dib br-pill ba ph4 pv2 cta">
+    <img class="mr2 v-mid" height="19px" src="{{ secure_asset('dist/img/l-arrow.svg') }}" alt="cta"> 
+    Go Back </a>
   </div>
 </section>
+
 
 @include('layouts.footer')
